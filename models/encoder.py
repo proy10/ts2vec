42af207a082e41143bef5ac76c6a6071e55dff72
Pan Rui <pan_rui@sjtu.edu.cn>
2023-11-28 13:48:09 +0800

    update

--- models/encoder.py
+++ models/encoder.py
@@ -1,8 +1,10 @@
 import torch
+import random
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
 from .dilated_conv import DilatedConvEncoder
+
 
 def generate_continuous_mask(B, T, n=5, l=0.1):
     res = torch.full((B, T), True, dtype=torch.bool)
@@ -20,8 +22,19 @@ def generate_continuous_mask(B, T, n=5, l=0.1):
             res[i, t:t+l] = False
     return res
 
+
 def generate_binomial_mask(B, T, p=0.5):
     return torch.from_numpy(np.random.binomial(1, p, size=(B, T))).to(torch.bool)
+
+
+def generate_bert_mask(B, T, p=0.15):
+    masked_indices = torch.bernoulli(torch.full((B, T), p)).bool()
+    indices_replaced = torch.bernoulli(torch.full((B, T), 0.8)).bool() & masked_indices
+    # prob_data = torch.bernoulli(torch.full((B, T), 0.5)).bool()
+    indices_random = torch.bernoulli(torch.full((B, T), 0.5)).bool() & masked_indices & ~indices_replaced
+    # indices_saved = masked_indices & ~indices_replaced & ~indices_random
+    return masked_indices, indices_replaced, indices_random
+
 
 class TSEncoder(nn.Module):
     def __init__(self, input_dims, output_dims, hidden_dims=64, depth=10, mask_mode='binomial'):
@@ -37,38 +50,53 @@ class TSEncoder(nn.Module):
             kernel_size=3
         )
         self.repr_dropout = nn.Dropout(p=0.1)
+        self.projection = nn.Linear(output_dims, input_dims)
         
-    def forward(self, x, mask=None):  # x: B x T x input_dims
+    def forward(self, x, mask_mode=None):  # x: B x T x input_dims
         nan_mask = ~x.isnan().any(axis=-1)
         x[~nan_mask] = 0
         x = self.input_fc(x)  # B x T x Ch
         
         # generate & apply mask
-        if mask is None:
+        if mask_mode is None:
             if self.training:
-                mask = self.mask_mode
+                mask_mode = self.mask_mode
             else:
-                mask = 'all_true'
+                mask_mode = 'all_true'
         
-        if mask == 'binomial':
+        if mask_mode == 'binomial':
             mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
-        elif mask == 'continuous':
+        elif mask_mode == 'continuous':
             mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
-        elif mask == 'all_true':
+        elif mask_mode == 'all_true':
             mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
-        elif mask == 'all_false':
+        elif mask_mode == 'all_false':
             mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
-        elif mask == 'mask_last':
+        elif mask_mode == 'mask_last':
             mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
             mask[:, -1] = False
-        
-        mask &= nan_mask
-        x[~mask] = 0
+        elif mask_mode == 'bert':
+            mask, indices_replaced, indices_random = generate_bert_mask(x.size(0), x.size(1))
+            mask = ~mask.to(x.device)
+            indices_replaced = indices_replaced.to(x.device)
+            indices_random = indices_random.to(x.device)
+            # indices_saved = indices_saved.to(x.device)
+
+        if mask_mode == 'bert':
+            x[indices_replaced] = 0
+            random_vector = torch.randint(len(x), x.shape, dtype=torch.float).to(x.device)
+            x[indices_random] = random_vector[indices_random]
+        else:
+            mask &= nan_mask
+            x[~mask] = 0
         
         # conv encoder
-        x = x.transpose(1, 2)  # B x Ch x T
-        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B(batch) x Ch(channel) x T(time)
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T, torch.Size([8, 320, 528])
+        # print("xshape\n", x.shape)
         x = x.transpose(1, 2)  # B x T x Co
-        
-        return x
+
+        enc_out = self.projection(x.detach())
+
+        return x, enc_out
         
